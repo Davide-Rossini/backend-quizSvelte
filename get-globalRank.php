10f53cc2013cb1accffd1c4d9c7b3a8517a639f7
steGiro05 <stefanogiroletti9@gmail.com>
2024-03-01 10:48:49 +0100

    globalRank settato

--- get-globalRank.php
+++ get-globalRank.php
@@ -5,24 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once(__DIR__ . '/protected/config.php');
 try {
-    $queryMaxScore = $db->prepare('SELECT MAX(punteggio) as maxScore FROM `punteggio` WHERE fkUtente=:id');
-    $queryMaxScore->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-    $queryMaxScore->execute();
-    $maxScore = $queryMaxScore->fetch();
-
-    $queryMinScore = $db->prepare('SELECT MIN(punteggio) as minScore FROM `punteggio` WHERE fkUtente=:id');
-    $queryMinScore->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-    $queryMinScore->execute();
-    $minScore = $queryMinScore->fetch();
     
-    $queryAvgScore = $db->prepare('SELECT ROUND(AVG(punteggio),2) as avgScore FROM `punteggio` WHERE fkUtente=:id');
-    $queryAvgScore->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-    $queryAvgScore->execute();
-    $avgScore = $queryAvgScore->fetch();
-
-    $result=array('max'=>$maxScore,'min'=>$minScore,'avg'=>$avgScore);
-
-    echo '{"status":1, "data":' . json_encode($result) . '}';
+    echo '{"status":1, "data":"ciao"}';
 } catch (PDOException $ex) {
     sendError('error executing query', __LINE__);
 }
