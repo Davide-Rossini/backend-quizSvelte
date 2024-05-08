a6cac0b0036c39cc33989e07d2977556172fc2fc
DavozGamer <davo.z.gam3r@gmail.com>
2024-05-08 21:34:07 +0200

    abilitata connessione porta 4306

--- protected/config.php
+++ protected/config.php
@@ -1,18 +1,23 @@
 <?php
 
-
 try {
     $dbUsername = 'root';
-    $dbPassword = '';
-    $dbConnection = 'mysql:host=localhost; dbname=5bi_backend_quizmaster; charset=utf8';
+    $dbPassword = ''; // Assicurati di inserire la password corretta se è impostata
+    $dbHost = 'localhost'; // Modifica questo se il database non è in esecuzione sulla stessa macchina
+    $dbPort = '4306'; // Porta del database
+    $dbName = '5bi_backend_quizmaster'; // Nome del tuo database
+    $dbCharset = 'utf8';
+
+    $dbConnection = "mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=$dbCharset";
+
     $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //allows to use try catch
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC //allows to use associatives arrays then converted to JSON
-        //json_encode [{},{}]
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Consente di utilizzare il blocco try-catch
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Consente di utilizzare gli array associativi, che poi verranno convertiti in JSON
     ];
 
     $db = new PDO($dbConnection, $dbUsername, $dbPassword, $options);
 } catch (PDOException $ex) {
-    echo '{"status":0, "message":"cannot connect to the db", "debug":' . __LINE__ . '}';
+    echo '{"status":0, "message":"Impossibile connettersi al database", "debug":' . __LINE__ . '}';
     exit();
 }
+?>
